95cb847da4b13f2011f007377e1e227104331c93
kirtimistry2001 <kirtimistry2001@yahoo.com>
2021-04-07 15:06:07 -0400

    Adding Entity to the Persistence Context using
    EntityManager to persist and track the changes.

--- src/main/java/ca/kirti/jpa/springdatajpademo/service/UserDAOService.java
+++ src/main/java/ca/kirti/jpa/springdatajpademo/service/UserDAOService.java
@@ -1,15 +1,38 @@
 package ca.kirti.jpa.springdatajpademo.service;
 
+import javax.persistence.EntityManager;
+import javax.transaction.Transactional;
+
+import org.springframework.stereotype.Repository;
+
 import ca.kirti.jpa.springdatajpademo.enity.User;
 
-import javax.persistence.EntityManager;
-
+/**
+ * This  class helps in string/accessing data 
+ * from the database (DAO), inspring this type of component known as Repository
+ * using entityManager
+ * @author Kirti
+ *
+ */
+@Repository //is component that helps in Accessing/Storing Data to database
+@Transactional //ability to declaratively control transaction (open transaction, close transaction)
 public class UserDAOService {
 
     private EntityManager entityManager;
 
     public long insert(User user) {
-        entityManager.persist(user);
+    	// example
+    /*	User user1 = new User("Roy", "ROLE_USER");
+    	User user2 = new User("Lina", "ROLE_ADMIN");
+    	//adding user1 to  Persistence Context using  entity manager to persist
+    	entityManager.persist(user1); // now user1 is managed and tracked by entity Manager
+    	/ 
+    	user1.setRole("ROLE_ADMIN"); //changes tracked by entity manager
+    	
+    	user2.setRole("ROLE_USER"); //Here user2 is not added to entity manger so it will not be tracked by entity manager
+    	*/
+    	
+        entityManager.persist(user); // persist and manage that is it now tracked by entity manager
         return user.getId();
     }
 }
