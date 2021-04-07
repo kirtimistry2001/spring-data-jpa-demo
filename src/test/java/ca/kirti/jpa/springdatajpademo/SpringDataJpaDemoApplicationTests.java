03041dbafe03cd5a00ded32696c6ded0ecefd95c
kirtimistry2001 <kirtimistry2001@yahoo.com>
2021-04-07 14:04:05 -0400

    Initial commit to Spring data JPA project

---
+++ src/test/java/ca/kirti/jpa/springdatajpademo/SpringDataJpaDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.kirti.jpa.springdatajpademo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataJpaDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
